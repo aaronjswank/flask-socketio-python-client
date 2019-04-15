968edfc5340fffbd3058c7bff03a42e34d090149
Swank, Aaron J <a.j.swank@grc.nasa.gov>
2019-04-15 13:46:13 -0400

    Note that gevent works.

--- server/app.py
+++ server/app.py
@@ -13,8 +13,10 @@ from wtforms.validators import InputRequired, Length
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
+# NOTE: when using eventlet, and python socketio.AsyncClient, messages are truncated.
 #async_mode = None
 async_mode = 'eventlet'
+#async_mode = 'gevent'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(32)
